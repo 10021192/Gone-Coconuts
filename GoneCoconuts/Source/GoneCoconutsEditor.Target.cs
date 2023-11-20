c0b9217e374f177bfd7512a5c71824868fb386bb
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2023-11-20 13:49:58 +0000

    forgot to change this shit

--- GoneCoconuts/Source/GoneCoconutsEditor.Target.cs
+++ GoneCoconuts/Source/GoneCoconutsEditor.Target.cs
@@ -9,7 +9,7 @@ public class GoneCoconutsEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 		ExtraModuleNames.Add("GoneCoconuts");
 	}
 }
