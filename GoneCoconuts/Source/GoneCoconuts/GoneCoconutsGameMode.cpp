00c12eb233a09644182d9a7dab1be13e181b7962
SkyLinek98 <andre.ferreira.enes@gmail.com>
2023-12-29 17:52:19 +0000

    added 3rd person base package + fx variety pack + attempt failed to spawn in 3rd person

--- GoneCoconuts/Source/GoneCoconuts/GoneCoconutsGameMode.cpp
+++ GoneCoconuts/Source/GoneCoconuts/GoneCoconutsGameMode.cpp
@@ -8,7 +8,7 @@ AGoneCoconutsGameMode::AGoneCoconutsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/ThirdPerson/Blueprints/Player_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
